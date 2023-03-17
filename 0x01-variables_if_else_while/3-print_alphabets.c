01c5702875693a27286dfa26be9825ec7ff6ccb4
Kimathi Gilbert <gilbertkimathi018@gmail.com>
2023-03-17 09:33:48 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet in lowercase and uppercase
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-char alp[52] = "letter_lu";
-	int i;
+        char letter;
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+        for (letter = 'a'; letter <= 'z'; letter++)
+                putchar(letter);
+
+        for (letter = 'A'; letter <= 'Z'; letter++)
+                putchar(letter);
+
+        putchar('\n');
+
+        return (0);
 }
